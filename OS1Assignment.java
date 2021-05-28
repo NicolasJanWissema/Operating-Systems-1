fb7708834ea7fdabcd93686264d22b915e59e54d
Nicolas Wissema <wssnic008@myuct.ac.za>
2021-05-28 13:17:27 +0200

    Code finished and submitted for marking.

--- OS1Assignment.java
+++ OS1Assignment.java
@@ -1,10 +1,13 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 //operating systems memory management code. Don't know what im doing yet.
 public class OS1Assignment {
     //Page table used. can be changed.
-    static final long[] PageTable= new long[]{2,4,1,7,3,5,6};
+    static final long[] pageTable= new long[]{2,4,1,7,3,5,6};
+    static final int pageOffset = 5;
 
+    //Begin main method
     public static void main(String[] args){
 
         //Check input argument.
@@ -20,9 +23,13 @@ public class OS1Assignment {
                     FileInputStream inputStream = new FileInputStream(args[0]);
                     )
             {
-
+                //Variables required for translation.
                 byte[] buffer = new byte[Byte.SIZE];
-                Long.toHexString(Long.parseLong(Long.toBinaryString(2),2));
+                String[] conversionTable = OS1Assignment.convertPageTable(pageTable);
+                FileWriter fileWriter = new FileWriter("output.txt");
+
+                //Begin translation
+                System.out.println("Beginning translation of addresses.");
                 while((inputStream.read(buffer))!=-1)
                 {
                     long value = 0;
@@ -30,24 +37,15 @@ public class OS1Assignment {
                     {
                         value += ((long) buffer[i] & 0xFF) << (8 * i);
                     }
-                    System.out.println(Long.toHexString(value)+":");
+                    //System.out.println("Converting "+OS1Assignment.formatter(Long.toHexString(value)));
                     String binaryStringValue = Long.toBinaryString(value);
                     binaryStringValue = OS1Assignment.fullBinary(binaryStringValue,12);
-
-
-                    System.out.println(binaryStringValue);
-                    System.out.println(binaryStringValue.substring(0,4));
-                    if(binaryStringValue.substring(0,5).compareTo("00000")==0){
-                        binaryStringValue = "00010"+binaryStringValue.substring(5,binaryStringValue.length());
-                    }
-                    if(binaryStringValue.substring(0,5).compareTo("00100")==0){
-                        binaryStringValue = "00011"+binaryStringValue.substring(5,binaryStringValue.length());
-                    }
-                    System.out.println(binaryStringValue);
-                    System.out.println(Long.toHexString(Long.parseLong(binaryStringValue,2)));
-                    System.out.println();
+                    binaryStringValue = conversionTable[(int)Long.parseLong(binaryStringValue.substring(0,5),2)] + binaryStringValue.substring(5,binaryStringValue.length());
+                    fileWriter.write("Translated virtual address "+OS1Assignment.formatter(Long.toHexString(value))+" to physical address "+OS1Assignment.formatter(Long.toHexString(Long.parseLong(binaryStringValue,2)))+"\n");
+                    //System.out.println("Converted to: "+OS1Assignment.formatter(Long.toHexString(Long.parseLong(binaryStringValue,2)))+"\n");
                 }
-                System.out.println();
+                fileWriter.close();
+                System.out.println("Translation finished. output.txt created displaying results.");
             }
             catch(Exception e){
                 e.printStackTrace();
@@ -62,5 +60,19 @@ public class OS1Assignment {
             binaryInput = "0"+binaryInput;
         }
         return(binaryInput);
+    }
+    private static String[] convertPageTable(long[] table){
+        String[] convertedTable = new String[table.length];
+        for(int i=0;i<table.length;i++){
+            convertedTable[i]=OS1Assignment.fullBinary(Long.toBinaryString(table[i]),pageOffset);
+        }
+        return (convertedTable);
+    }
+    private static String formatter(String input){
+        int usedHex = input.length()/2+ input.length()%2;
+        for(int i=0;i<7-usedHex;i++){
+            input = "0"+input;
+        }
+        return ("0x"+input.toUpperCase());
     }
 }
